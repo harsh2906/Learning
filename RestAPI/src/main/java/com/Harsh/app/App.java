dbe382fb62a9374a05c788978daed81a63ff790b
harsh2906 <harshmehta.in@gmail.com>
2020-06-12 15:44:19 +0530

    first

---
+++ RestAPI/src/main/java/com/Harsh/app/App.java
@@ -0,0 +1,13 @@
+package com.Harsh.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ RestAPI/src/test/java/com/Harsh/app/ATest.java
@@ -0,0 +1,18 @@
+package com.Harsh.app;
+
+import org.testng.annotations.Test;
+
+public class ATest {
+
+@Test
+public void a()
+{
+	System.out.println("1");
+}
+
+@Test
+public void b()
+{
+	System.out.println("2");
+}
+}
